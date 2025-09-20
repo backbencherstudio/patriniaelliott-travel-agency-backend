6cead5fe93a573f7bebbb65db0042df88bb3a8e1
Anik Hossain <anik.wdev@gmail.com>
2025-09-20 17:28:07 +0600

    wip

--- src/modules/admin/payments/payments.controller.ts
+++ src/modules/admin/payments/payments.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../../auth/guards/jwt-auth.guard';
+import { RolesGuard } from '@/src/common/guard/role/roles.guard';
+import { Request } from 'express';
+import { PaymentsService } from './payments.service';
 
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('payments')
-export class PaymentsController {}
+export class PaymentsController {
+    constructor(private paymentService: PaymentsService) { }
+
+    @ApiOperation({ summary: 'Get transactions by user' })
+    @Get('/transactions')
+    async transactions(@Req() req: Request,) {
+        return this.paymentService.getTransactions()
+    }
+}
